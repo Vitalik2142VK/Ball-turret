347d2f8cd50787530e4e0a6a1e00b2c057c841c1
Vitalii <lennab@mail.ru>
2025-12-11 14:10:35 +0300

    In process

--- Assets/Scripts/Level/EndlessLevelPlanner.cs
+++ Assets/Scripts/Level/EndlessLevelPlanner.cs
@@ -5,7 +5,9 @@ public class EndlessLevelPlanner : MonoBehaviour, ILevelActorsPlanner
 {
     [SerializeField] private WaveRepository _wavesWithBonuses;
     [SerializeField] private WaveRepository[] _waveRepositories;
-    [SerializeField, Min(10)] private int _bonusWavesLimit;
+    [SerializeField] private WaveRepository[] _hardWaveRepositories;
+    [SerializeField, Min(10)] private int _bonusWavesLimit = 10;
+    [SerializeField, Min(20)] private int _standartWavesLimit = 20;
 
     private void OnValidate()
     {
@@ -18,6 +20,13 @@ public class EndlessLevelPlanner : MonoBehaviour, ILevelActorsPlanner
         foreach (var waveRepository in _waveRepositories)
             if (waveRepository == null)
                 throw new NullReferenceException($"{_waveRepositories} has null elements");
+
+        if (_hardWaveRepositories == null || _hardWaveRepositories.Length == 0)
+            throw new InvalidOperationException(nameof(_hardWaveRepositories));
+
+        foreach (var waveRepository in _hardWaveRepositories)
+            if (waveRepository == null)
+                throw new NullReferenceException($"{_hardWaveRepositories} has null elements");
     }
 
     public int WavesCount => int.MaxValue;
@@ -34,13 +43,36 @@ public class EndlessLevelPlanner : MonoBehaviour, ILevelActorsPlanner
 
     public IWaveActorsPlanner GetWaveActorsPlanner(int waveNumber)
     {
+        if (waveNumber > _standartWavesLimit)
+            return GetHardWave(waveNumber);
+        else
+            return GetStandartWave(waveNumber);
+    }
+
+    private IWaveActorsPlanner GetStandartWave(int waveNumber)
+    {
         int modifiedWaveNumber = waveNumber % WaveRepository.WaveDivider;
 
         if (waveNumber < _bonusWavesLimit)
             if (_wavesWithBonuses.HasWaveNumber(modifiedWaveNumber))
                 return _wavesWithBonuses.GetWaveActorsPlanner(modifiedWaveNumber);
-        
+
         foreach (var waveRepository in _waveRepositories)
+            if (waveRepository.HasWaveNumber(modifiedWaveNumber))
+                return waveRepository.GetWaveActorsPlanner(modifiedWaveNumber);
+
+        throw new InvalidOperationException("There is no suitable repository in with this wave number");
+    }
+
+    private IWaveActorsPlanner GetHardWave(int waveNumber)
+    {
+        int modifiedWaveNumber = waveNumber % WaveRepository.WaveDivider;
+
+        if (waveNumber < _bonusWavesLimit)
+            if (_wavesWithBonuses.HasWaveNumber(modifiedWaveNumber))
+                return _wavesWithBonuses.GetWaveActorsPlanner(modifiedWaveNumber);
+
+        foreach (var waveRepository in _hardWaveRepositories)
             if (waveRepository.HasWaveNumber(modifiedWaveNumber))
                 return waveRepository.GetWaveActorsPlanner(modifiedWaveNumber);
 
